db91c7cf8a09471340667f1a4e7e8b03d918630b
LCC47 <lccnet47@gmail.com>
2025-03-08 15:09:26 +0800

    Update content.js

--- content.js
+++ content.js
@@ -2,10 +2,8 @@ chrome.runtime.sendMessage({ action: "yourAction" }, function(response) {
     if (response) {
         if (response.result === "success") {
             console.log("成功回應:", response.data);
-            // 處理成功回應
         } else if (response.result === "error") {
             console.error("錯誤回應:", response.message);
-            // 處理錯誤回應
         }
     } else {
         console.error("未收到回應");
