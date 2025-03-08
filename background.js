ff836e35016f8340a8a815c19dd01fe6fc29434a
LCC47 <lccnet47@gmail.com>
2025-03-08 15:09:10 +0800

    Update background.js

--- background.js
+++ background.js
@@ -2,8 +2,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "yourAction") {
     // 執行一些非同步任務
     setTimeout(function() {
-      sendResponse({ result: "success" }); // 發送回應
+      sendResponse({ result: "success" });
     }, 1000);
-    return true; // 表示將非同步回應
+    return true; 
   }
 });
